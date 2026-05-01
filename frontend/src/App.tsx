9778c84c70bd4091137895dcc8689664063e46cf
Neil Brags Guzman <neilbragsg@gmail.com>
2026-05-01 19:37:37 +0800

    added components folder

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,8 +3,9 @@ export default function App() {
     return (
         <div className="flex min-h-screen items-center justify-center bg-gray-100">
             <h1 className="text-4xl font-extrabold text-blue-600 drop-shadow-sm">
-                Hello World
+                ECG V1
             </h1>
         </div>
     );
 }
+
---
+++ frontend/src/components/Menu.tsx
@@ -0,0 +1,5 @@
+export default function menu() {
+    return (
+        <div className=""></div>
+    )
+}
