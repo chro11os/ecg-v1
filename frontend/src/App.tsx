6b819f3d129c4443cd0928ed8b272d5d067f6e6c
Neil Brags Guzman <neilbragsg@gmail.com>
2026-05-01 22:04:53 +0800

    prettify

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,11 @@
 // src/App.tsx
-export default function App() {
-    return (
-        <div className="flex min-h-screen items-center justify-center bg-gray-100">
-            <h1 className="text-4xl font-extrabold text-blue-600 drop-shadow-sm">
-                ECG V1
-            </h1>
-        </div>
-    );
-}
 
+export default function App() {
+	return (
+		<div className="flex min-h-screen items-center justify-center bg-gray-100">
+			<h1 className="text-4xl font-extrabold text-blue-600 drop-shadow-sm">
+				ECG V1
+			</h1>
+		</div>
+	);
+}
