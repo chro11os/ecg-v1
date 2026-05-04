dc7c979e3006ae502f50176d0371a1ef949fbba0
Neil Brags Guzman <neilbragsg@gmail.com>
2026-05-05 04:16:56 +0800

    ScanHistory

--- frontend/src/components/MenuCard.tsx
+++ frontend/src/components/MenuCard.tsx
@@ -2,7 +2,7 @@ function MenuCard() {
     return (
         <div className="flex flex-col items-start gap-2 shadow-2xl text-4xl p-7 rounded-md bg-brand-surface">
             <button className="text-brand-dark hover:text-brand-accent">scan</button>
-        </div >
+        </div>
     );
 }
 
--- frontend/src/components/ScanHistory.tsx
+++ frontend/src/components/ScanHistory.tsx
@@ -16,7 +16,24 @@ function ScanHistory() {
 
     return (
         <div className="max-w-md mx-auto p-6 bg-brand-surface rounded-md shadow-2xl ">
-            )
+            <form onSubmit={addItem} className="flex gap-2 mb-6">
+                <input
+                    type = "text"
+                    value = {inputValue}
+                    onChange ={(e) => setInputValue(e.target.value)}
+                    placeholder = "Add New Task"
+                    className = "flex-1 px-4 py-2 border-gray-300 rounded-md focus:outline-none focus:ring-blue-500"
+                />
+                <button
+                    type="submit"
+                    className="px-4 py-2 bg-blue-600 text-white rounded-md "
+                >
+                    
+                </button>   
+            </form>
+            
+        </div>
+    )
 }
 
             export default ScanHistory;
