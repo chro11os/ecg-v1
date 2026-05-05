00f53345d64c13e3532ceba8ea29d4c783e7b14c
Neil Brags Guzman <neilbragsg@gmail.com>
2026-05-06 00:28:10 +0800

    frontend setup: done

--- frontend/src/components/MenuCard.tsx
+++ frontend/src/components/MenuCard.tsx
@@ -1,7 +1,9 @@
+import ScanButton from "./ScanButton.tsx";
+
 function MenuCard() {
     return (
-        <div className="flex flex-col items-start gap-2 shadow-2xl text-4xl p-7 rounded-md bg-brand-surface">
-            <button className="text-brand-dark hover:text-brand-accent">scan</button>
+        <div className="max-w-auto">
+            <ScanButton />
         </div>
     );
 }
