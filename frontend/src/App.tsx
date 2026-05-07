90864a2693f3ad389a7f107b2b7f95f508660f97
chro11os <neilbragsg@gmail.com>
2026-05-07 17:59:20 +0800

    reset code llm

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,6 @@
-import MenuCard from "./components/MenuCard";
-import ScanHistory from "./components/ScanHistory";
-
 function App() {
     return (
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-4 p-4">
-            <div className=" p-6 ">
-                <ScanHistory />
-            </div>
-            <div className=" p-6 ">
-                <MenuCard />
-            </div>
-            <div className=" p-6 ">
-
-            </div>
-        </div>
+        
     )
 }
 
