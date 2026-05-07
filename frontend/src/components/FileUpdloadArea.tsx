ac617dd322bf1cded9008cc900ad97ec65f33a0d
chro11os <neilbragsg@gmail.com>
2026-05-08 02:33:06 +0800

    added: FileUploadDragArea - Place para mag upload ng files

--- frontend/src/components/FileUpdloadArea.tsx
+++ frontend/src/components/FileUpdloadArea.tsx
@@ -1,25 +1,31 @@
-import { useCallback } from 'react'
-import { useDropzone } from "react-dropzone";
+import React from 'react';
+import { useDropzone, FileWithPath } from 'react-dropzone';
 
-function FileUploadArea() {
+interface FileUploadAreaProps {
 
-    const onDrop = useCallback(acceptedFiles: => {
-        // do something with the files 
-    }, [])
+}
 
-    const { getRootProps, getInputProps, isDragActive } =
-        useDropzone({ onDrop })
+function FileUploadArea(props: FileUploadAreaProps) {
+    const { acceptedFiles, getRootProps, getInputProps } = useDropzone();
+
+    const files = acceptedFiles.map((file: FileWithPath) => (
+        <li key={file.path}>
+            {file.path} - {file.size} bytes
+        </li>
+    ));
 
     return (
-        <div {...getRootProps()}>
-            <input {...getInputProps()} />
-            {
-                isDragActive ?
-                    <p>Drop the files here ...</> :
-                    <p>Drang 'n' drop some files here, or click to select files</p>
-            }
-        </div>
-    )
+        <section className="container">
+            <div {...getRootProps({ className: 'dropzone' })}>
+                <input {...getInputProps()} />
+                <p>Drag n drop some files here, or click to select files</p>
+            </div>
+            <aside>
+                <h4>Files</h4>
+                <ul>{files}</ul>
+            </aside>
+        </section>
+    );
 }
 
 export default FileUploadArea;
