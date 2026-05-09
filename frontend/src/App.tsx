735669625b78d0ef3a1673ce308e7aefb9d592bd
chro11os <neilbragsg@gmail.com>
2026-05-09 14:44:43 +0800

    induced comma

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,11 @@
 import DiagnosisDashboard from "./components/DiagnosisDashboard";
 import FileUploadArea from "./components/FileUpdloadArea";
+
 function App() {
 	return (
-		<div className="flex flex-col gap-10 bg-slate-900 min-h-screen pb-20">
+		<div>
+			<FileUploadArea />
 			<DiagnosisDashboard />
-			<div className="px-8 max-w-6xl mx-auto w-full">
-				<FileUploadArea />
-			</div>
 		</div>
 	)
 }
